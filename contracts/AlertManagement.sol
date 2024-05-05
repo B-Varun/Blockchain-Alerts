243ddea04783712d0c4b3e60512e7e63ff8a9f86
B-Varun <bvarun662@gmail.com>
2024-05-04 23:58:38 -0700

    Added code for AlertMAnagement Smart Contract

--- contracts/AlertManagement.sol
+++ contracts/AlertManagement.sol
@@ -4,6 +4,15 @@ pragma solidity >=0.5.16;
 
 contract AlertManagement{
 
+    // Count of currently open alerts
+    int alert_Count = 0;
+
+    // Array to store alerts
+    Alert[] public alerts;
+
+    // Event is raised when a new alert is created
+    event AlertCreated(string desc);
+
     enum Alert_Status{
         Open, 
         Closed
@@ -25,6 +34,36 @@ contract AlertManagement{
         Alert_Status status;
     }
 
-    
+
+    // Returns the alert type constant using the user entered string
+    function get_AlertType_from_string(string memory alertType) private pure returns(Alert_Type){
+    if( keccak256(abi.encodePacked((alertType))) == keccak256(abi.encodePacked(("Fire"))) )
+        return Alert_Type.Fire_Alert;
+    else if( keccak256(abi.encodePacked((alertType))) == keccak256(abi.encodePacked(("Health"))) )
+        return Alert_Type.Health_Emergency_Alert;
+    if( keccak256(abi.encodePacked((alertType))) == keccak256(abi.encodePacked(("Theft"))) )
+        return Alert_Type.Theft_Alert;                
+    }
+
+
+    function setAlert(string memory title, string memory description, string memory recipient_Address, Alert_Type alertType) private{
+        alert_Count++;
+        Alert memory alert = Alert({
+            alertId : alert_Count,
+            title : title,
+            description : description,
+            recipient_Address : recipient_Address,
+            type_Of_Alert : alertType,
+            responder_Address : address(0),
+            status : Alert_Status.Open
+        });
+        alerts.push(alert);
+        emit AlertCreated(alert.title);
+    }
+
+    // Create a new Alert using this method
+    function createAlert(string memory title, string memory description, string memory recipient_Address, string memory alertType) public {
+        setAlert(title, description, recipient_Address, get_AlertType_from_string(alertType));
+    }
 
 }
