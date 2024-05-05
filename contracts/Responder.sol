8ffc1780cbc0845ded75145c4ad7af87389ac615
B-Varun <bvarun662@gmail.com>
2024-05-05 00:07:57 -0700

    Imported AlertManagement to access the Alert Struct in responder

--- contracts/Responder.sol
+++ contracts/Responder.sol
@@ -2,6 +2,12 @@
 // pragma solidity >=0.8.0 <=0.9.0;
 pragma solidity >=0.5.16;
 
+import "./AlertManagement.sol";
+
 contract Responder{
-    
+    //  Array to store the Alert ID's of the responder closed alerts
+    AlertManagement.Alert[] public respondedAlerts;
+
+
+
 }
